8c82e910f248b286a3ce809b7387b6e7f1973e17
Anil Singh <anilsuyal97153.com>
2023-11-21 01:18:25 +0530

    Unused files deleted

--- Secure Chat/secure-chat-app/src/SignOut.js
+++
@@ -1,9 +0,0 @@
-import { auth } from '../firebase';
-
-const SignOut = () => {
-  return (
-    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
-  );
-};
-
-export default SignOut;
